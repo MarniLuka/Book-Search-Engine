7b72a2bc25580df2d19cf4ca5b1acb66ef4fdcb6
Marni Luka <marnixluka@gmail.com>
2023-06-11 18:21:00 -0400

    added queries file to utils folder

---
+++ client/src/utils/queries.js
@@ -0,0 +1,16 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+    query me {
+        me {
+            _id, username, email,
+            savedBooks {
+                bookId,
+                authors,
+                image,
+                description,
+                title,
+                link,
+        }
+    }
+`;
